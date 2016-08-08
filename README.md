0717b7f70734b75c6338e3dd988a4ed86451eb01
KIMTUNG\Tung <kimtung@megaads.dev>
2016-08-08 09:52:36 +0700

    upgrade class Megalogger

--- README.md
+++ README.md
@@ -9,18 +9,26 @@
 ##Usage:
 
 ```javascript
-var megalogger = require('megalogger');
+var Megalogger = require('megalogger');
 
  // your apiKey
  var apiKey = 'asefas_efjhj-afsd64sf';
-
+ var source = 'Your source';
+ var initData = {
+            apiKey: apiKey,
+            source: source
+        };
  // Log
  var dataLog = [{data: "Message test"}];
 
  // level for log include: info, debug, warning, error, critical 
  var level = 'info';
+ 
+ // init instance of class Megalogger
+ var logger = new Megalogger(initData);
+
  // push log
-megalogger.log(apiKey, dataLog, level, 'source');
+ logger.log(dataLog, level);
  
 ```
 
--- index.js
+++ index.js
@@ -1,45 +1,47 @@
 var jwt = require('jsonwebtoken');
 var http = require('http');
+module.exports = MegaLogger;
+function MegaLogger(initData) {
+    this.source = initData.source;
+    this.token = jwt.sign({audience: initData.apiKey}, 'megadev_secret', {algorithm: 'HS512'});
 
-function log(apiKey, data, level, source) {
-    var token = jwt.sign({audience: apiKey}, 'megadev_secret', {algorithm: 'HS512'});
-    var d = new Date();
-    var n = d.getTime();
-    var dataLog = {
-        token: token,
-        type: 'request',
-        data: data,
-        level: level,
-        meta: {
-            language: 'Javascript'
-        },
-        time: n,
-        source: source
+    this.log = function (data, level) {
+        console.log(this.token);
+        var d = new Date();
+        var n = d.getTime();
+        var dataLog = {
+            token: this.token,
+            type: 'request',
+            data: data,
+            level: level,
+            meta: {
+                language: 'Javascript'
+            },
+            time: n,
+            source: this.source
 
-    };
-    var strData = JSON.stringify(dataLog);
-    var options = {
-        host: '192.168.1.172',
-        port: 8161,
-        path: '/api/message?destination=queue://logger',
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Content-Length': Buffer.byteLength(strData)
-        }
-    };
+        };
+        var strData = JSON.stringify(dataLog);
+        var options = {
+            host: '192.168.1.172',
+            port: 8161,
+            path: '/api/message?destination=queue://logger',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': Buffer.byteLength(strData)
+            }
+        };
 
-    var req = http.request(options, function (res) {
-        console.log('STATUS: ' + res.statusCode);
-        console.log('HEADERS: ' + JSON.stringify(res.headers));
-        res.setEncoding('utf8');
-        res.on('data', function (chunk) {
-            console.log('BODY: ' + chunk);
+        var req = http.request(options, function (res) {
+            console.log('STATUS: ' + res.statusCode);
+            console.log('HEADERS: ' + JSON.stringify(res.headers));
+            res.setEncoding('utf8');
+            res.on('data', function (chunk) {
+                console.log('BODY: ' + chunk);
+            });
         });
-    });
-    req.write(strData);
-    req.end();
+        req.write(strData);
+        req.end();
+    };
 }
-module.exports = {
-    log: log
-};
